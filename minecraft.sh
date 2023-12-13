629f9dc6fbc5b02a1da213cc77d4bdfee60b9c53
foxytouxxx <143593532+foxytouxxx@users.noreply.github.com>
2023-12-13 18:37:03 +0100

    Create minecraft.sh

---
+++ minecraft.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+# Clear the screen
+clear
+
+# Do Not Change !!!! (Credits)
+echo "
+#######################################################################################
+#
+#                                  Foxytoux
+#
+#                           Copyright (C) 2023 - 2024
+#
+#
+#######################################################################################"
+
+# Ask the user for the Minecraft server script URL
+read -p "Please enter the URL of the Minecraft server script (press Enter to use the default URL): " server_url
+server_url=${server_url:-"https://piston-data.mojang.com/v1/objects/5b868151bd02b41319f54c8d4061b8cae84e665c/server.jar"}
+
+# Download the Minecraft server script
+wget -O server.jar $server_url
+
+# Download openjdk-17-jdk
+sudo apt-get update
+sudo apt-get install openjdk-17-jdk -y
+
+# Ask the user for the amount of RAM to allocate
+read -p "How much RAM would you like to allocate to the Minecraft server (in GB)? " ram_amount
+
+# Modify the Java command with the user's response
+java_command="java -Xms${ram_amount}G -Xmx${ram_amount}G -jar server.jar nogui"
+
+# Launch the Minecraft server
+$java_command
+
+# Modify the eula.txt file to set 'true'
+echo "eula=true" > eula.txt
+
+# Restart the Minecraft server with the new configurations
+$java_command
